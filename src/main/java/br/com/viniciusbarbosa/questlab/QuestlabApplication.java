527fac32c9c10e91b67c7dd2548c06474d355d8d
vini-barbo <vinicius2508@hotmail.com>
2025-03-13 21:48:37 -0300

    first commit

--- src/main/java/br/com/viniciusbarbosa/questlab/QuestlabApplication.java
+++ src/main/java/br/com/viniciusbarbosa/questlab/QuestlabApplication.java
@@ -1,4 +1,4 @@
-package br.dev.viniciusbarbosa.questlab;
+package br.com.viniciusbarbosa.questlab;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/br/com/viniciusbarbosa/questlab/QuestlabApplicationTests.java
+++ src/test/java/br/com/viniciusbarbosa/questlab/QuestlabApplicationTests.java
@@ -1,4 +1,4 @@
-package br.dev.viniciusbarbosa.questlab;
+package br.com.viniciusbarbosa.questlab;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
